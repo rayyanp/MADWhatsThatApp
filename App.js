ed609584532a363c199d0bf9aa42445e0342c1ed
Rayyan Patel <rayyanp2@gmail.com>
2023-03-06 22:53:15 +0000

    Added navigation for Login and Registration Screens

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MAD WhatsThatApp!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const AuthStack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+      <NavigationContainer>
+        <AuthStack.Navigator initialRouteName="Login">
+          <AuthStack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <AuthStack.Screen name="Register" component={Register} options={{headerShown: false}} />
+        </AuthStack.Navigator>
+      </NavigationContainer>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
