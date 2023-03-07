f7da6b50cbd31ce91058d3bf2838f1166b4676cb
Rayyan Patel <rayyanp2@gmail.com>
2023-03-07 11:10:34 +0000

    Styling completed for Registration screen

--- Components/Register.js
+++ Components/Register.js
@@ -136,3 +136,57 @@ render() {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '80%',
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingLeft: 10,
+    marginBottom: 20,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 20,
+  },
+  createAccountButton: {
+    marginTop: 10,
+    backgroundColor: 'white',
+    borderColor: '#2980b9',
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    alignItems: 'center',
+  },
+  createAccountText: {
+    color: '#2980b9',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor:'#2980b9',
+    padding: 10,
+    borderRadius: 8,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+});
