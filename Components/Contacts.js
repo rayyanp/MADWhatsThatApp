6d0caff55a020843343df03a3c7365b8ef0c10b6
Rayyan Patel <rayyanp2@gmail.com>
2023-03-09 11:37:22 +0000

    Flatlist changed to section list so contacts are grouped by first name.

--- Components/Contacts.js
+++ Components/Contacts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, SectionList, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Contacts extends Component {
   static navigationOptions = {
@@ -90,21 +91,19 @@ export default class Contacts extends Component {
         console.log(err)
     })
   }
-  
+      
   removeContact = async (contact) => {
     const { id } = contact;
     fetch(`http://localhost:3333/api/1.0.0/user/`+id+`/contact`, {
       method: 'DELETE',
       headers: {
         'X-Authorization': await AsyncStorage.getItem("whatsthat_session_token"),
-        'Accept': 'application/json',
+        'Content-Type': 'application/json',
       },
     })
       .then((response) => {
         if (response.status === 200) {
-          const { contacts } = this.state;
-          const updatedContacts = contacts.filter((c) => c.id !== id);
-          this.setState({ contacts: updatedContacts, error: null });
+          this.getContacts(); // call getContacts() to update the list of contacts
         } else if (response.status === 400) {
           throw new Error("You can't remove yourself as a contact");
         } else if (response.status === 401) {
@@ -150,93 +149,182 @@ export default class Contacts extends Component {
         this.setState({ error: error.message });
       });
   };
-
-  renderItem = ({ item }) => (
-    <TouchableOpacity
-      style={styles.contactContainer}
-    >
-      <Text style={styles.contactName}>{item.name}</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.deleteButton}
-          onPress={() => this.removeContact(item)}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.blockButton}
-          onPress={() => this.blockUser(item)}
-        >
-          <Text style={styles.buttonText}>Block</Text>
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
-  );
   
 
-  render() {
-    const { contacts, error } = this.state;
+render() {
+  const { contacts, error, photos } = this.state;
 
-    return (
-      <View style={styles.container}>
-        {error && <Text style={styles.error}>{error}</Text>}
-        <FlatList
-          data={contacts}
-          renderItem={this.renderItem}
+  function orderContacts(contacts) {
+    const order = {};
+
+    // group the contacts by their first letter of the name
+    contacts.forEach(contact => {
+      const firstLetter = contact.name.charAt(0).toUpperCase();
+      if (!order[firstLetter]) {
+        order[firstLetter] = [];
+      }
+      order[firstLetter].push(contact);
+    });
+
+    // convert the groups object to an array of sections
+    const sections = Object.keys(order).sort().map(letter => ({
+      title: letter,
+      data: order[letter],
+    }));
+
+    return sections;
+  }
+
+  const orderedContacts = orderContacts(contacts);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Contacts</Text>
+        <TouchableOpacity
+          style={styles.viewBlockedButton}
+          onPress={() => this.props.navigation.navigate('Blocked')}
+        >
+          <Text style={styles.viewBlockedText}>View Blocked Users</Text>
+        </TouchableOpacity>
+      </View>
+      {error ? (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{error}</Text>
+        </View>
+      ) : (
+        <SectionList
+          sections={orderedContacts}
+          renderItem={({ item }) => (
+            <View style={styles.contactContainer}>
+              <View style={styles.photoContainer}>
+                {photos[item.id] ? (
+                  <Image source={{ uri: photos[item.id] }} style={styles.photo} />
+                ) : (
+                  <Text style={styles.noPhotoText}>No photo</Text>
+                )}
+              </View>
+              <Text style={styles.contactName}>{item.name}</Text>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.deleteButton}
+                  onPress={() => this.removeContact(item)}
+                >
+                  <Icon name="delete" size={20} color="#FFF" />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.blockButton}
+                  onPress={() => this.blockUser(item)}
+                >
+                  <Text style={styles.buttonText}>Block</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.sectionHeader}>{title}</Text>
+          )}
           keyExtractor={(item) => item.id}
         />
-      </View>
-    );
-  }
-}
-
+      )}
+    </View>
+  );
+          }
+        }  
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#FFF',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ECECEC',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  viewBlockedButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    backgroundColor: '#6B55E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  viewBlockedText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  errorContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  errorText: {
+    fontSize: 18,
+    color: '#FF0000',
+  },
+  sectionHeader: {
+    backgroundColor: '#F5F5F5',
+    padding: 10,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#333',
+  },
   contactContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    borderBottomColor: '#E0E0E0',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
     borderBottomWidth: 1,
+    borderBottomColor: '#ECECEC',
   },
   contactName: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
-    color: '#333333',
-  },
-  deleteButton: {
-    backgroundColor: '#FF3B30',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 4,
-  },
-  deleteButtonText: {
-    color: '#FFFFFF',
-    fontSize: 14,
-  },
-  error: {
-    color: '#FF3B30',
-    fontSize: 16,
-    textAlign: 'center',
-    marginTop: 16,
+    color: '#333',
   },
   buttonContainer: {
     flexDirection: 'row',
   },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 14,
+  deleteButton: {
+    backgroundColor: '#FF0000',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginRight: 10,
   },
   blockButton: {
-    backgroundColor: '#007AFF',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 4,
-    marginLeft: 8,
+    backgroundColor: '#6B55E6',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  photoContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+    },
+  photo: {
+    width: 75,
+    height: 75,
+    borderRadius: 40,
+    },
+  noPhotoText: {
+    fontSize: 12,
+    color: '#999',
   },
 });
