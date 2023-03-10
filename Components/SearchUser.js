012d34a87c3c744030f2967266e4da548543ffe7
Rayyan Patel <rayyanp2@gmail.com>
2023-03-10 14:08:33 +0000

    Implemented search functionality/api interaction. Need to add render code so users can submit their search result and view results.

--- Components/SearchUser.js
+++ Components/SearchUser.js
@@ -1,17 +1,52 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, ScrollView, SectionList, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default class SearchUser extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>SearchUser Screen</Text>
-      </View>
-    );
-  }
+export default class SearchUsers extends Component {
+  state = {
+    query: '',
+    searchIn: 'all',
+    limit: 20,
+    offset: 0,
+    users: [],
+  };
+
+  searchUsers = async () => {
+    const { query, searchIn, limit, offset } = this.state;
+
+    if (!query) {
+      this.setState({ error: 'Please enter a search query' });
+      return;
+    }
+
+    fetch(`http://localhost:3333/api/1.0.0/search?q=`+query+`&search_in=`+searchIn+`&limit=`+limit+`&offset=`+offset, {
+      method: 'GET',
+      headers: {
+        'X-Authorization': await AsyncStorage.getItem("whatsthat_session_token"),
+        'Accept': 'application/json',
+      },
+    })
+      .then(response => {
+        if (response.status === 200) {
+          console.log('OK');
+        } else if (response.status === 400) {
+          console.error('Bad Request');
+        } else if (response.status === 401) {
+          console.error('Unauthorized');
+        } else if (response.status === 500) {
+          console.error('Server Error');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.length === 0) {
+          this.setState({ error: 'No results' });
+        } else {
+          this.setState({ users: data,});
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 }
