edec39be16f3fbc6eb870d1a33b7c9927ab7e8b0
Rayyan Patel <rayyanp2@gmail.com>
2023-03-13 15:36:17 +0000

    Amended chat item style.

--- Components/ChatList.js
+++ Components/ChatList.js
@@ -189,6 +189,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
   },
   chatInfo: {
     flex: 1,
