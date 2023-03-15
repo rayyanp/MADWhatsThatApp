4ba51f90c8d2583fe4995ebd8eb615cfed900367
Rayyan Patel <rayyanp2@gmail.com>
2023-03-15 13:16:31 +0000

    Added api interaction for chats

--- Components/ChatScreen.js
+++ Components/ChatScreen.js
@@ -9,6 +9,29 @@ export default class ChatScreen extends Component {
   });
  }
 
+ fetchChatData = async () => {
+  const { chatId } = this.props.route.params;
+
+  try {
+    const response = await fetch(
+      `http://localhost:3333/api/1.0.0/chat/`+chatId+`?limit=20&offset=0`,
+      {
+        headers: {
+          'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token'
+          ),
+          Accept: 'application/json',
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+    }
+  } catch (error) {
+    console.error('Error fetching chat data:', error);
+  }
+};
+
  
   render() {
     return (
