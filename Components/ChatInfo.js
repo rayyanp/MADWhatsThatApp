c42a09e76987bd0c4301ea783b83ce5bd19baf4f
Rayyan Patel <rayyanp2@gmail.com>
2023-03-16 13:06:27 +0000

    added api interaction for fetching chatdata to get members of the chat.

--- Components/ChatInfo.js
+++ Components/ChatInfo.js
@@ -1,17 +1,49 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default class ChatInfo extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>ChatInfo screen</Text>
-      </View>
-    );
+
+export class ChatInfoScreen extends Component {
+  state = {
+    members: [],
+    error: null,
+  };
+
+  componentDidMount() {
+    this.props.navigation.addListener('focus', () => {
+    const chatId = this.props.route.params.chatId;
+    this.fetchChatData(chatId);
+  });
   }
+
+  fetchChatData = async (chatId) => {
+    try {
+      const response = await fetch(`http://localhost:3333/api/1.0.0/chat/`+chatId, {
+        headers: {
+          'X-Authorization': await AsyncStorage.getItem("whatsthat_session_token"),
+          Accept: 'application/json',
+        },
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        this.setState({
+          chatName: data.name,
+          members: data.members,
+          error: null,
+        });
+      } else if (response.status === 400) {
+        throw new Error('Bad Request');
+      } else if (response.status === 401) {
+        throw new Error('Unauthorized');
+      } else if (response.status === 404) {
+        throw new Error('Not Found');
+      } else {
+        throw new Error('Server Error');
+      }
+    } catch (error) {
+      console.error(error);
+      this.setState({ error: error.message});
+    }
+  };
 }
